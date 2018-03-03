429d054ce2959affaebc724d4e58f86af8dbf7b5
Weidong Guo <weidongguo@live.com>
2018-03-03 03:21:34 -0800

    retrieved cookies

--- popup.js
+++ popup.js
@@ -1,1 +1,21 @@
-console.log("popup.js loaded");
+chrome.tabs.getSelected(null, function(tab) {
+      console.log('log');
+      console.log(tab);
+      
+      chrome.runtime.onMessage.addListener(
+			  function(request, sender, sendResponse) {
+			  	console.log(request);
+			  	request.split(' ').map(function(kv) {
+				  	d3.select('#content').append('p')
+				  		.text(kv);
+			  	});
+			  }
+			);
+
+      chrome.tabs.executeScript(tab.id, {code: `
+	      chrome.runtime.sendMessage(document.cookie, function(response) {
+				  console.log(response);
+				});
+      `});
+
+});
